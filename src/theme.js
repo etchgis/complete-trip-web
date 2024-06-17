37d875e042c35d766ce2b877dc28cef876ece9ed
JesseGIS <jesse@etchgis.com>
2024-06-17 12:35:06 -0400

    kiosk screen resolution update

--- src/theme.js
+++ src/theme.js
@@ -154,7 +154,7 @@ const customTheme = {
     base: '0em',
     sm: '30em',
     md: '48em',
-    lg: mode === 'kiosk' ? '58em' : '62em',
+    lg: mode === 'kiosk' ? '50em' : '62em',
     xl: '80em',
     '2xl': '96em',
   }
